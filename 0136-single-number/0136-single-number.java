81d477e1d0adc7083646a4d30838c0669f5744fa
nirajgharate <nirajgharate.dev@gmail.com>
2025-07-02 23:27:55 +0530

    Time: 1 ms (99.91%), Space: 46.8 MB (5.52%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,20 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
-          for (int i = 0; i < nums.length; i++) {
-            int count = 0;
+          int result = 0; // start with 0
 
-            // Count occurrences of nums[i]
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    count++;
-                }
-            }
-
-            // If the count is 1, this is our single element
-            if (count == 1) {
-                return nums[i];
-            }
+        for (int num : nums) {
+            result ^= num; // XOR each number with the result
         }
-        return -1;
+
+        return result;
     }
 }
