27d0dcc32561d60d00d2734d5a20ed1af26fbe34
nirajgharate <nirajgharate.dev@gmail.com>
2025-07-08 21:58:43 +0530

    Time: 0 ms (100%), Space: 43.4 MB (18.6%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -6,7 +6,7 @@ class Solution {
             int mid = left + (right-left)/2;
             if(nums[mid]==target){
                 return mid;
-            }else if(nums[mid]<mid){
+            }else if(nums[mid]<target){
                 left = mid+1;
 
             }else{
