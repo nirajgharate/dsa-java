3a3f6d3827a3574b4325e07862ea9e04dbd4ba6f
nirajgharate <nirajgharate.dev@gmail.com>
2025-07-25 23:07:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] freq = new int[26]; // to count letters in magazine
+
+        // Count letters in magazine
+        for (int i = 0; i < magazine.length(); i++) {
+            freq[magazine.charAt(i) - 'a']++;
+        }
+
+        // Check if ransomNote can be constructed
+        for (int i = 0; i < ransomNote.length(); i++) {
+            char c = ransomNote.charAt(i);
+            if (freq[c - 'a'] == 0) {
+                return false;
+            }
+            freq[c - 'a']--;
+        }
+
+        return true;
+        }
+    }
+}
