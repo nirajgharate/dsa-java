776cbac3307c871c5a036cf4ef1e7d345578e847
nirajgharate <nirajgharate.dev@gmail.com>
2026-08-13 23:42:43 +0530

    Time: 3 ms (94.79%), Space: 43 MB (76.1%) - LeetHub

---
+++ 2465-number-of-distinct-averages/2465-number-of-distinct-averages.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int distinctAverages(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length-1;
+        
+        while(left < right){
+            int sum = nums[left] + nums[right];
+
+            set.add(sum);
+
+            left++;
+            right--;
+        }
+        return set.size();
+    }
+}
